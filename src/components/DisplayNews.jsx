d4e364e30d0f4a4476f7e32fc3adab61058d06a5
Eevan <eevan.redon@gmail.com>
2020-03-30 20:06:29 +0200

    adds new white section for free content

--- src/components/DisplayNews.jsx
+++ src/components/DisplayNews.jsx
@@ -18,87 +18,91 @@ const DisplayNews = props => {
   useEffect(() => {
     loadFreeArticle();
   }, []);
-  
+
 
   return (
     <>
       <Navbar />
       <div className="backgroundgrey">
         <div className="main">
-            <div className="item-a">
-              <DisplayCurrentArticle />
-            </div>
-            <div className="item-b">
-              <DisplaySideArticles />
-            </div>
-            <div id="seperator" className="item-c">
-              <div id="border-top">
-                {props.sideArticles !== null && (
-                  <>
-                    <h4>{props.sideArticles.articles[0].title}</h4>
-                    <img id="featured-img" src={props.sideArticles.articles[0].image} />
-                    <h5>{props.sideArticles.articles[0].body}</h5>
-                    <p>{props.sideArticles.articles[0].category}</p>
-                  </>
-                )}
-              </div>
-            </div>
-            <div id="seperator" className="item-d">
-              <div id="border-top">
-                {props.sideArticles !== null && (
-                  
-                  <>
-                    <h4>{props.sideArticles.articles[1].title}</h4>
-                    <img id="featured-img" src={props.sideArticles.articles[1].image} />
-                    <h5>{props.sideArticles.articles[1].body}</h5>
-                    <p>{props.sideArticles.articles[1].category}</p>
-                  </>
-                )}
-              </div>
-            </div>
-            <div className="item-e">
-              <div id="border-top">
+          <div className="item-a">
+            <DisplayCurrentArticle />
+          </div>
+          <div className="item-b">
+            <DisplaySideArticles />
+          </div>
+          <div id="seperator" className="item-c">
+            <div id="border-top">
               {props.sideArticles !== null && (
-                  <>
-                    <h4>{props.sideArticles.articles[2].title}</h4>
-                    <img id="featured-img" src={props.sideArticles.articles[2].image} />
-                    <h5>{props.sideArticles.articles[2].body}</h5>
-                    <p>{props.sideArticles.articles[2].category}</p>
-                  </>
-                )}
-              </div>
-            </div>
-            <div className="item-f">
-              <div id="weather">
                 <>
-                  <Weather />
+                  <h4>{props.sideArticles.articles[0].title}</h4>
+                  <img id="featured-img" src={props.sideArticles.articles[0].image} />
+                  <h5>{props.sideArticles.articles[0].body}</h5>
+                  <p>{props.sideArticles.articles[0].category}</p>
                 </>
-              </div>
-            </div>
-            <div className="item-g">
-              <div className="advertising">
-                ADVERTISING
-              </div>
-            </div>
-            <div className="item-h">
-              <div id="border-top">
-                {props.sideArticles !== null && (
-                  <>
-                    <h1>Free Read</h1>
-                    <img className="free-read-img" src={freeContent[1]} />
-                  </>
-                )}
-              </div>
-            </div>
-            <div className="item-i">
-              {props.sideArticles !== null && (
-                <div className="free-read">
-                  <h1>{freeContent[0].title}</h1>
-                  <p>{freeContent[0].body}</p>
-                  <p id="cat" >{freeContent[0].category}</p>
-                </div>
               )}
             </div>
+          </div>
+          <div id="seperator" className="item-d">
+            <div id="border-top">
+              {props.sideArticles !== null && (
+
+                <>
+                  <h4>{props.sideArticles.articles[1].title}</h4>
+                  <img id="featured-img" src={props.sideArticles.articles[1].image} />
+                  <h5>{props.sideArticles.articles[1].body}</h5>
+                  <p>{props.sideArticles.articles[1].category}</p>
+                </>
+              )}
+            </div>
+          </div>
+          <div className="item-e">
+            <div id="border-top">
+              {props.sideArticles !== null && (
+                <>
+                  <h4>{props.sideArticles.articles[2].title}</h4>
+                  <img id="featured-img" src={props.sideArticles.articles[2].image} />
+                  <h5>{props.sideArticles.articles[2].body}</h5>
+                  <p>{props.sideArticles.articles[2].category}</p>
+                </>
+              )}
+            </div>
+          </div>
+          <div className="item-f">
+            <div id="weather">
+              <>
+                <Weather />
+              </>
+            </div>
+          </div>
+          <div className="item-g">
+            <div className="advertising">
+              ADVERTISING
+              </div>
+          </div>
+        </div>
+      </div>
+      <div className="whitebackground">
+        <div className="main2">
+          <div className="item-h">
+            <div id="border-top">
+              {props.sideArticles !== null && (
+                <>
+                  <h2>FREE READ</h2>
+                  <img className="free-read-img" src={freeContent[1]} />
+                </>
+              )}
+            </div>
+          </div>
+          <div className="item-i">
+            {props.sideArticles !== null && (
+              <div className="free-read">
+                <h1>{freeContent[0].title}</h1>
+                <p>{freeContent[0].body}</p>
+                <p id="cat" >{freeContent[0].category}</p>
+              </div>
+            )}
+          </div>
         </div>
       </div>
       <Footer />
